37bb8469f4a5c875b6ac4524de971b7a8028fc50
E-bot-rgb <zebastianericsson@gmail.com>
2026-06-07 21:20:19 +0200

    fix: open CORS for all origins

--- FolvestAPI/Program.cs
+++ FolvestAPI/Program.cs
@@ -48,11 +48,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
-        policy.WithOrigins(
-            "http://localhost:5173",
-            "https://localhost:5173",
-            "http://localhost:5174",
-            "https://localhost:5174")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
